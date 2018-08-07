5b666d066ba07f1927cbf492b7dd69e1696b194d
Kendra Kelly and Nikki Wong <kendra@>
2018-08-07 09:56:06 -0700

    add in text, toggle action in javascript.

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $(".clickable1").click(function() {
+    $(".show-answer1").slideToggle();
+  })
+  $(".clickable2").click(function() {
+    $(".show-answer2").slideToggle();
+  })
+  $(".clickable3").click(function() {
+    $(".show-answer3").slideToggle();
+  })
+  $(".clickable4").click(function() {
+    $(".show-answer4").slideToggle();
+  })
+  $(".clickable5").click(function() {
+    $(".show-answer5").slideToggle();
+  });
+});
